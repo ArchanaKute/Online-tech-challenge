276db07776296cda0b7cb80c275e7fadb04b7ab9
ArchanaKute <48614530+ArchanaKute@users.noreply.github.com>
2021-09-23 16:18:44 +0530

    Update vpc-subnet.tf

--- vpc-subnet.tf
+++ vpc-subnet.tf
@@ -20,3 +20,70 @@ module "demo-vpc-subnet" {
     },
     ]
 }
+  
+/**************************************************
+Creating Cloud Router for NAT
+**************************************************/
+module "test-nat-uswe1-01" {
+  source       = "git::https://source.developers.google.com/p/demo-proj/r/demo-bucket//modules/network?ref=tags/v1.2.3"
+  router_name  = "test-router-nat-uswe1-01"
+  network_name = module.demo-test-network.id
+  region_name  = "us-west1"
+  project_id   = "demo-vpc-proj"
+}
+
+/**************************************************
+Creating Cloud NAT
+**************************************************/
+module "test-uswe1-01" {
+  source                              = "git::https://source.developers.google.com/p/demo-proj/r/demo-bucket//modules/network?ref=tags/v1.2.3"
+  project_id                          = "demo-vpc-proj"
+  region_name                         = "us-west1"
+  nat_name                            = "test-nat-uswe1-01"
+  router_name                         = "test-router-nat-uswe1-01"
+  source_subnetwork_ip_ranges_to_nat  = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+  enable_endpoint_independent_mapping = false
+  min_ports_per_vm                    = 300
+  log_config_enable                   = true
+  log_config_filter                   = "ALL"
+}
+
+  /**************************************************
+  Creating GCE
+***************************************************/
+
+module "test-mod-vm" {
+
+  source              = "git::https://source.developers.google.com/p/lpgprj-gss-n-iacbst-gl-01/r/lpgrep-gss-iac-mod-npr-01//modules/compute-instance?ref=tags/v1.1.1"
+  project_id          = "demo-vm-proj"
+  instance_name       = "demo-vm"
+  machine_type        = "e2-standard-16"
+  zone_name           = "us-east1-b"
+  deletion_protection = true
+
+  network_interface = [
+    {
+      network_name       = null
+      external_ip_needed = false
+      subnetwork_name    = "projects/demo-vpc-proj/regions/us-east1/subnetworks/demo-subnet"
+      network_ip         = null
+      access_config      = {}
+      alias_ip_range     = []
+  }]
+
+  boot_disk = {
+    image = "projects/demo-vm-proj/global/images/cvm-0006"
+    size  = "50"
+    type  = "pd-ssd"
+  }
+  labels = {
+    "platform-name" = "test"
+  }
+  shielded_instance_config = {
+    "enable_secure_boot" = "true"
+  }
+  service_account = {
+    email  = "test-service-account@demo-vpc-proj.iam.gserviceaccount.com"
+    scopes = ["cloud-platform"]
+  }
+}
